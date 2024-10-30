8533274c79b8d56fa70cbe4d48351e5e2d74d3a5
Rui Domingues <rdomingues@broadvoice.com>
2024-10-30 23:57:15 +0000

    fix: update description and sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
   return (
     <section className="space-y-6 pb-8 md:pb-12 md:pt-10 lg:py-32">
       <div className="container mt-6 flex max-w-5xl flex-col items-center gap-4 text-center xl:mt-0">
-        <h1 className="text-2xl capitalize sm:text-5xl md:text-6xl lg:text-7xl">
-          Building <span className="font-code text-yellow-300">UIs</span> that{" "}
-          <span className="font-code text-primary">scales</span>
+        <h1 className="text-4xl font-semibold capitalize sm:text-5xl md:text-6xl lg:text-7xl">
+          Building <span className="text-yellow-300">UIs</span> that{" "}
+          <span className="text-primary">scales</span>
         </h1>
         <p className="max-w-2xl leading-normal text-muted-foreground sm:text-xl sm:leading-8">
           {siteConfig.description}
@@ -19,7 +19,7 @@ export default function Home() {
             href="/blog"
             className={cn(
               buttonVariants({ size: "lg", variant: "secondary" }),
-              "border",
+              "border"
             )}
           >
             My Blog
--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -11,21 +11,32 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { useSelectedLayoutSegment } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 export function AppSidebar() {
+  const segment = useSelectedLayoutSegment();
+
   return (
     <Sidebar>
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupLabel>@ruijadom</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {NAV_LIST.map((item) => (
-                <SidebarMenuItem key={item.label + item.path}>
+          <SidebarGroupContent className="mt-14">
+            <SidebarMenu className="space-y-2">
+              {NAV_LIST.map(({ label, path, icon: Icon }) => (
+                <SidebarMenuItem
+                  key={label + path}
+                  className={cn(
+                    "font-normal hover:text-primary transition-colors flex items-center",
+                    `/${segment}` === path
+                      ? "text-primary"
+                      : "text-muted-foreground"
+                  )}
+                >
                   <SidebarMenuButton asChild>
-                    <a href={item.path}>
-                      <item.icon />
-                      <span>{item.label}</span>
+                    <a href={path}>
+                      <span className="text-3xl">{label}</span>
                     </a>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
