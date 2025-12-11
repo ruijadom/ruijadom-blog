9ee70bd469c2f1c54754ee85d2fc83b4345b8072
Rui Domingues <rdomingues@broadvoice.com>
2025-12-11 21:25:58 +0000

    feat: spaceship

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
-import { buttonVariants } from "@/components/ui/button";
 import { siteConfig } from "@/config/site";
-import { cn } from "@/lib/utils";
-import Link from "next/link";
 import { RocketShip } from "@/components/rocket-ship";
 
 export default function Home() {
   return (
     <>
       <RocketShip />
-      <section className="relative z-[50] space-y-6 pb-8 md:pb-12 md:pt-10 lg:py-32">
+      <section className="relative z-50 space-y-6 pb-8 md:pb-12 md:pt-10 lg:py-32">
         <div className="container mt-6 flex max-w-5xl flex-col items-center gap-4 text-center xl:mt-0">
           <h1 className="text-4xl font-semibold capitalize sm:text-5xl md:text-6xl lg:text-7xl">
             Building <span className="text-yellow-300">UIs</span> that{" "}
